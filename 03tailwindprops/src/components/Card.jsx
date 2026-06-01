e994934dc88e10c5133c51998b0b02c8be803d41
SHIV24116 <singhshivendra1945@gmail.com>
2026-06-01 23:33:30 +0530

    03updated

--- 03tailwindprops/src/components/Card.jsx
+++ 03tailwindprops/src/components/Card.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function Card({username, btnText="visit me"}) {   //function Card(props) .....we access  variables as ....props.username......or directly declare variable inside {} in props
     console.log(username);
   return (
-    <div className="relative h-[400px] w-[300px] rounded-md ">
+    <div className="relative h-400px w-300px rounded-md ">
        <img
-        src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+        src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
         alt="AirMax Pro"
         className="z-0 h-full w-full rounded-md object-cover"
         />
